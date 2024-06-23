a4308ca60f0062d7389204ba72a6f7cc3c7b2773
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-06-23 17:47:39 -0300

    adicionado middleware de paginacao em todas as rotas.

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -2,10 +2,13 @@ import NaoEncontrado from "../erros/NaoEncontrado.js";
 import { autores } from "../models/index.js";
 
 class AutorController {
-  static listarAutores = async (req, res) => {
+  static listarAutores = async (req, res, next) => {
     try {
-      const autoresResultado = await autores.find();
-      res.status(200).json(autoresResultado);
+      const autoresResultado = autores.find();
+
+      req.resultado = autoresResultado;
+
+      next();
     } catch (erro) {
       res.status(500).json({ message: "Erro interno no servidor" });
     }
--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,31 +1,16 @@
 import { autores, livros } from "../models/index.js";
-import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import NaoEncontrado from "../erros/NaoEncontrado.js";
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1, ordenacao = "_id: -1" } = req.query;
 
-      let [campoOrdenacao, ordem] = ordenacao.split(":");
+      const buscaLivros = livros.find();
 
-      limite = parseInt(limite);
-      pagina = parseInt(pagina);
-      ordem = parseInt(ordem);
+      req.resultado = buscaLivros;
 
-      if (limite > 0 && pagina > 0) {
-        const livrosResultado = await livros.find()
-          .sort({ [campoOrdenacao]: ordem })
-          .skip((pagina -1) * limite)
-          .limit(limite)
-          .populate("autor")
-          .exec();
-
-        res.status(200).json(livrosResultado);
-      } else {
-        next(new RequisicaoIncorreta());
-      }
+      next();
 
     } catch (erro) {
       next(erro);
@@ -99,11 +84,13 @@ class LivroController {
       const busca = await processaBusca(req.query);
 
       if (busca !== null) {
-        const livrosResultado = await livros
+        const livrosResultado = livros
           .find(busca)
           .populate("autor");
 
-        res.status(200).send(livrosResultado);
+        req.resultado = livrosResultado;
+
+        next();
 
       } else {
         res.status(200).send([]);
